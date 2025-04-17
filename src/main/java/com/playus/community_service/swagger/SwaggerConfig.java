e7f92fab6fd58f81a6f0fef816c93c3380f0b929
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-04-17 18:25:14 +0900

    chore: swagger 설정 추가

---
+++ src/main/java/com/playus/community_service/swagger/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.playus.community_service.swagger;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@OpenAPIDefinition(info = @Info(
+	title = "PlayUs Community Service API",
+	description = "PlayUs 커뮤니티 관리 서비스 API 문서",
+	version = "v1.0.0"))
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openApi() {
+		String jwt = "JWT";
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT");
+		Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+			.name(jwt)
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT")
+		);
+
+		Server server = new Server();
+		server.setUrl("/");
+
+		return new OpenAPI()
+			.servers(List.of(server))
+			.addSecurityItem(securityRequirement)
+			.components(components);
+	}
+}
