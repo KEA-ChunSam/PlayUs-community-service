2bb7b33ce79a1a09948af1b42c46d6a06bbedb4d
han16935 <han1693516@gmail.com>
2025-04-18 16:14:49 +0900

    chore/CommunityServiceApplication 위 주석 제거

--- src/main/java/com/playus/community_service/CommunityServiceApplication.java
+++ src/main/java/com/playus/community_service/CommunityServiceApplication.java
@@ -3,7 +3,6 @@ package com.playus.community_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//
 @SpringBootApplication
 public class CommunityServiceApplication {
 
