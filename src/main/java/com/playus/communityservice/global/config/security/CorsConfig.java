8ddf162ff7c947ceba7c10eb425421e186af940d
chaemin <cheamin0038@gachon.ac.kr>
2025-06-02 22:08:46 +0900

    Chore: CORS 설정 추가 - 오리진 명시 - CORS credentials 설정 변경

--- src/main/java/com/playus/communityservice/global/config/security/CorsConfig.java
+++ src/main/java/com/playus/communityservice/global/config/security/CorsConfig.java
@@ -2,27 +2,42 @@ package com.playus.communityservice.global.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
+    private static final List<String> ALLOWED_ORIGINS = List.of(
+            "https://web.playus.o-r.kr",
+            "https://api.playus.o-r.kr",
+            "http://localhost:3000"
+    );
+
     @Bean("corsConfigurationSource")
-    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource() {
+
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of(
-                "*"
-        ));
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
+        configuration.setAllowedOrigins(ALLOWED_ORIGINS);
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration
+                .setAllowCredentials(true);
+        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
 
-        configuration.setAllowCredentials(true);
+        configuration.addAllowedMethod(HttpMethod.GET.name());
+        configuration.addAllowedMethod(HttpMethod.POST.name());
+        configuration.addAllowedMethod(HttpMethod.PUT.name());
+        configuration.addAllowedMethod(HttpMethod.DELETE.name());
+        configuration.addAllowedMethod(HttpMethod.OPTIONS.name());
 
-        org.springframework.web.cors.UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
